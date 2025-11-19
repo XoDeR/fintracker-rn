739b8b9511f8ff54517756510c77462e1552ca02
XoDeR <xoder.vs@gmail.com>
2025-11-19 19:35:01 +0100

    enabled env vars

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Test: server works");
-})
+});
+
+console.log("Port: ", process.env.PORT);
 
 app.listen(5001, () => {
   console.log("server running on PORT:5001");
