86f299ee22b059a7bf2ee2c84eaa243951911acd
XoDeR <xoder.vs@gmail.com>
2025-11-19 19:25:42 +0100

    test route created to see if server works

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 
-const app = express()
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Test: server works");
+})
 
 app.listen(5001, () => {
   console.log("server running on PORT:5001");
-})
+});
