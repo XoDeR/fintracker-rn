fcc516b3b4ae46fdb74af901eaf92af6ee12bf6a
XoDeR <xoder.vs@gmail.com>
2025-11-21 18:42:59 +0100

    refactored init db to a separate file

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,3 +4,23 @@ import "dotenv/config"; // does the same as import dotenv + call dotenv.config()
 
 // SQL connection
 export const sql = neon(process.env.DATABASE_URL);
+
+export async function initDb() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10,2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    // DECIMAL(10,2) total 10 digits, 2 digits after the decimal point
+
+    console.log("DB initted successfully")
+  } catch (error) {
+    console.log("DB init error:", error);
+    process.exit(1); // 1 === status code FAILURE
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
+import { initDb } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js";
@@ -15,28 +15,8 @@ app.use(express.json)
 
 const PORT = process.env.PORT || 5001;
 
-async function initDb() {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions(
-      id SERIAL PRIMARY KEY,
-      user_id VARCHAR(255) NOT NULL,
-      title VARCHAR(255) NOT NULL,
-      amount DECIMAL(10,2) NOT NULL,
-      category VARCHAR(255) NOT NULL,
-      created_at DATE NOT NULL DEFAULT CURRENT_DATE
-    )`;
-
-    // DECIMAL(10,2) total 10 digits, 2 digits after the decimal point
-
-    console.log("DB initted successfully")
-  } catch (error) {
-    console.log("DB init error:", error);
-    process.exit(1); // 1 === status code FAILURE
-  }
-}
-
-// For testing
-app.get("/", (req, res) => {
+// For testing, health check
+app.get("/health", (req, res) => {
   res.send("Test: server works");
 });
 
