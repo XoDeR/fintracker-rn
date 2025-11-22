7746294716de06913238cb8ccc32e02f7623c2cb
XoDeR <xoder.vs@gmail.com>
2025-11-22 16:16:34 +0100

    example using styles in RN

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,18 +1,13 @@
 import { Image } from "expo-image";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "green",
-      }}
+      style={styles.container}
     >
-      <Text style={{ color: "blue" }}>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.heading}>Edit app/index.tsx to edit this screen.</Text>
       <Link href={"/about"}>About</Link>
       <Image
         source={{ uri: "https://images.unsplash.com/photo-1587829741301-dc798b83add3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5Ym9hcmR8ZW58MHx8MHx8fDA%3D" }}
@@ -25,3 +20,16 @@ export default function Index() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "purple",
+  },
+  heading: {
+    color: "blue",
+    fontSize: 20,
+  },
+});
